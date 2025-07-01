828bfb86e2573f7edb1002b4091747b968a20bb7
ferrofy <team.ferrofy@gmail.com>
2025-07-01 17:15:43 +0530

    Download Material Push

---
+++ Downloads/Investments.py
@@ -0,0 +1,49 @@
+#######################################| Libraries |#######################################
+import time
+
+#######################################| Public Person Data |#######################################
+# Will Upload Soon
+
+
+#######################################| Investments |#######################################
+
+Investments = [
+
+#==============| Dec 2024 |==============#
+
+    {
+        "Person": "Tanav",
+        "Amount": 610 ,
+        "Spents": ["CT Pool"]
+        "Date": [7,"Dec","2024"]
+    },
+    {
+        "Person": "VPX",
+        "Amount": 510,
+        "Spents": ["CT Pool"]
+        "Date": [7,"Dec","2024"]
+    },
+    {
+        "Person": "AKX",
+        "Amount": 20,
+        "Spents": ["CT Pool"]
+        "Date": [7,"Dec","2024"]
+    },
+    {
+        "Person": "KMX",
+        "Amount": 20,
+        "Spents": ["CT Pool"]
+        "Date": [7,"Dec","2024"]
+    },
+
+#==============| Jan 2025 |==============#
+
+    {
+        "Person": "KMX",
+        "Amount": 20,
+        "Spents": ["CT Pool"]
+        "Date": [7,"Dec","2024"]
+    },
+
+]
+
