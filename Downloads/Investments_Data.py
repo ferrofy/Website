cb7612edaa09a95a7bf234c494d8b1197e0404ae
ferrofy <team.ferrofy@gmail.com>
2025-07-08 04:55:42 +0530

    Updated A Little Investment Data

---
+++ Downloads/Investments_Data.py
@@ -0,0 +1,159 @@
+#######################################| Investments |#######################################
+
+Investments = [
+
+#==============| Dec 2024 |==============#
+
+    {
+        "Person": "Tanav",
+        "Amount": 610 ,
+        "Spents": { "CT Pool" : 610 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [7,"Dec","2024"]
+    },
+    {
+        "Person": "VPX",
+        "Amount": 510,
+        "Spents": { "CT Pool" : 510 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [7,"Dec","2024"]
+    },
+    {
+        "Person": "AKX",
+        "Amount": 20,
+        "Spents": { "CT Pool" : 20 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [7,"Dec","2024"]
+    },
+    {
+        "Person": "KMX",
+        "Amount": 20,
+        "Spents": { "CT Pool" : 20 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [7,"Dec","2024"]
+    },
+
+#==============| Feb 2025 |==============#
+
+    {
+        "Person": "VPX",
+        "Amount": 880,
+        "Spents": { "CT Pool" : 880 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [19,"Feb","2025"]
+    },
+    {
+        "Person": "PPX",
+        "Amount": 200,
+        "Spents": { "CT Pool" : 200 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [19,"Feb","2025"]
+    },
+    {
+        "Person": "AKX",
+        "Amount": 60,
+        "Spents": { "CT Pool" : 60 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [19,"Feb","2025"]
+    },
+    {
+        "Person": "VPX",
+        "Amount": 60,
+        "Spents": { "CT Pool" : 60 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [19,"Feb","2025"]
+    },
+
+#==============| Mar 2025 |==============#
+
+    {
+        "Person": "VPX",
+        "Amount": 860,
+        "Spents": { "CT Pool" : 860 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [7,"Mar","2025"]
+    },
+    {
+        "Person": "PPX",
+        "Amount": 310,
+        "Spents": { "CT Pool" : 310 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [7,"Mar","2025"]
+    },
+    {
+        "Person": "Angel",
+        "Amount": 30,
+        "Spents": { "CT Pool" : 30 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [7,"Mar","2025"]
+    },
+    {
+        "Person": "PPX",
+        "Amount": 10,
+        "Spents": { "CT Pool" : 0 , "Direct_Buy" : 0 , "Near_Chain" : 10 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [11,"Mar","2025"]
+    },
+    {
+        "Person": "KRX",
+        "Amount": 500,
+        "Spents": { "CT Pool" : 0 , "Direct_Buy" : 0 , "Near_Chain" : 500 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [17,"Mar","2025"]
+    },
+    {
+        "Person": "PiPX",
+        "Amount": 120,
+        "Spents": { "CT Pool" : 0 , "Direct_Buy" : 0 , "Near_Chain" : 120 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [18,"Mar","2025"]
+    },
+    {
+        "Person": "PPX",
+        "Amount": 50 ,
+        "Spents": { "CT Pool" : 0 , "Direct_Buy" : 0 , "Near_Chain" : 50 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [24,"Mar","2025"]
+    },
+    {
+        "Person": "PPX",
+        "Amount": 50 ,
+        "Spents": { "CT Pool" : 0 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [30,"Mar","2025"]
+    },
+
+#==============| Apr 2025 |==============#
+
+    {
+        "Person": "FerroFy",
+        "Amount": 10 ,
+        "Spents": { "CT Pool" : 10 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [1,"Apr","2025"]
+    },
+    {
+        "Person": "VPX",
+        "Amount": 1121 ,
+        "Spents": { "CT Pool" : 1105 , "Direct_Buy" : 0 , "Near_Chain" : 16 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [1,"Apr","2025"]
+    },
+    {
+        "Person": "FerroFy",
+        "Amount": 22 ,
+        "Spents": { "CT Pool" : 22 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 0 , "Fees" : 0 },
+        "Date": [4,"Apr","2025"]
+    },
+    {
+        "Person": "VPX",
+        "Amount": 109,
+        "Spents": { "CT Pool" : 87 , "Direct_Buy" : 0 , "Near_Chain" : 11 , "eGold" : 0 , "Fees" : 11 },
+        "Date": [10,"Apr","2025"]
+    },
+    {
+        "Person": "FerroFy",
+        "Amount": 110 ,
+        "Spents": { "CT Pool" : 90 , "Direct_Buy" : 0 , "Near_Chain" : 0 , "eGold" : 20 , "Fees" : 0 },
+        "Date": [15,"Apr","2025"]
+    }
+
+
+
+
+]
+
+#######################################| Basic Structure |#######################################
+
+# Error At Paste , and Uncomment , Fill All Data Then No Error
+
+#==============| Month Year |==============#
+
+# {
+#         "Person": "",
+#         "Amount":  ,
+#         "Spents": { "CT Pool" :  , "Direct_Buy" :  , "Near_Chain" :  , "eGold" :  , "Fees" :  },
+#         "Date": [,"",""]
+#     },
